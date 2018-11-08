201f68c567e7a43d1e94ddbf63912354fa72c095
torikraju <torikraju@hotmail.com>
2018-11-09 00:52:10 +0600

    046 Connecting React Native to Redux

--- myApp/index.js
+++ myApp/index.js
@@ -1,7 +1,20 @@
 /** @format */
 
 import {AppRegistry} from 'react-native';
+import {Provider} from 'react-redux';
+import React from 'react';
+
 import App from './App';
 import {name as appName} from './app.json';
+import configureStore from './src/store/configureStore';
 
-AppRegistry.registerComponent(appName, () => App);
+const store = configureStore();
+
+const RNRedux = () => (
+    <Provider store={store}>
+        <App/>
+    </Provider>
+);
+
+
+AppRegistry.registerComponent(appName, () => RNRedux);
---
+++ myApp/src/store/reducers/places.js
@@ -0,0 +1,47 @@
+import * as actionTypes from '../actions/actionTypes';
+
+const initialState = {
+    places: [],
+    selectedPlace: null
+};
+
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case actionTypes.ADD_PLACE:
+            return {
+                ...state,
+                places: state.places.concat({
+                    key: Math.random(),
+                    name: action.placeName,
+                    image: {
+                        uri: 'http://offroadbangladesh.com/wp-content/uploads/2015/02/Amiakum-waterfall-626x365.jpg'
+                    }
+                })
+            };
+        case actionTypes.DELETE_PLACE:
+            return {
+                ...state,
+                places: state.places.filter(place => {
+                    return place.key !== state.selectedPlace.key;
+                }),
+                selectedPlace: null
+            };
+        case actionTypes.SELECT_PLACE:
+            return {
+                ...state,
+                selectedPlace: state.places.find(place => {
+                    return place.key === action.placeKey;
+                })
+            };
+        case actionTypes.DESELECT_PLACE:
+            return {
+                ...state,
+                selectedPlace: null
+            };
+        default:
+            return state;
+    }
+};
+
+export default reducer;
