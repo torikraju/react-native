325695ea72821608a068325956578ebf8b28f9ac
torikraju <torikraju@hotmail.com>
2018-11-08 18:27:15 +0600

    035 Adding a Modal

--- myApp/App.js
+++ myApp/App.js
@@ -1,15 +1,17 @@
 import React, {Component} from 'react';
 import {StyleSheet, View} from 'react-native';
 
+
 import PlaceInput from './src/component/PlaceInput/PlaceInput';
 import PlaceList from './src/component/PlaceList/PlaceList';
-
+import PlaceDetail from './src/component/PlaceDetail/PlaceDetail';
 
 
 type Props = {};
 export default class App extends Component<Props> {
     state = {
-        places: []
+        places: [],
+        selectedPlace: null
     };
 
     placeAddedHandler = placeName => {
@@ -19,30 +21,50 @@ export default class App extends Component<Props> {
                     key: Math.random(),
                     name: placeName,
                     image: {
-                        uri:'http://offroadbangladesh.com/wp-content/uploads/2015/02/Amiakum-waterfall-626x365.jpg'
+                        uri: 'http://offroadbangladesh.com/wp-content/uploads/2015/02/Amiakum-waterfall-626x365.jpg'
                     }
                 })
             };
         });
     };
 
-    placeDeletedHandler = key => {
+    placeSelectedHandler = key => {
+        this.setState(prevState => {
+            return {
+                selectedPlace: prevState.places.find(place => {
+                    return place.key === key;
+                })
+            };
+        });
+
+    };
+    placeDeletedHandler = () => {
         this.setState(prevState => {
             return {
                 places: prevState.places.filter((place) => {
-                    return place.key !== key;
-                })
+                    return place.key !== prevState.selectedPlace.key;
+                }),
+                selectedPlace: null
             };
+        });
+    };
+    modalClosedHandler = () => {
+        this.setState({
+            selectedPlace: null
         });
     };
 
     render() {
         return (
             <View style={styles.container}>
+                <PlaceDetail
+                    selectedPlace={this.state.selectedPlace}
+                    onItemDeleted={this.placeDeletedHandler}
+                    onModalClose={this.modalClosedHandler}/>
                 <PlaceInput onPlaceAdded={this.placeAddedHandler}/>
                 <PlaceList
                     places={this.state.places}
-                    onItemDeleted={this.placeDeletedHandler}/>
+                    onItemSelected={this.placeSelectedHandler}/>
             </View>
         );
     }
