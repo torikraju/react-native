307cfdde33e975d332ca7e5ee883cecc585472dd
torikraju <torikraju@hotmail.com>
2018-11-08 12:30:02 +0600

    024 Styling - Understanding the Basics

--- myApp/App.js
+++ myApp/App.js
@@ -31,7 +31,8 @@ export default class App extends Component<Props> {
         return (
             <View style={styles.container}>
                 <TextInput
-                    style={{width: 300, borderColor: 'black', borderWidth: 1}}
+                    style={{width: 300}}
+                    placeholder='An awesome place'
                     onChangeText={this.placeNameChangeHandler}
                     value={this.state.placeName}/>
             </View>
@@ -42,7 +43,8 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
+        padding: 26,
+        justifyContent: 'flex-start',
         alignItems: 'center',
         backgroundColor: '#F5FCFF',
     },
