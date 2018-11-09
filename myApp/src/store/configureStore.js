5d8259e04a2cac68a1d87a545275b330efd99c44
torikraju <torikraju@hotmail.com>
2018-11-09 22:33:26 +0600

    053 Debugging Redux

--- myApp/src/store/configureStore.js
+++ myApp/src/store/configureStore.js
@@ -1,4 +1,4 @@
-import {createStore, combineReducers} from 'redux';
+import { createStore, combineReducers, compose } from 'redux';
 import placesReducers from './reducers/places';
 
 
@@ -6,8 +6,15 @@ const rootReducer = combineReducers({
     places: placesReducers
 });
 
+let composeEnhancers = compose;
+
+if (__DEV__) {
+    composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+}
+
 const configureStore = () => {
-    return createStore(rootReducer)
+    return createStore(rootReducer, composeEnhancers());
 };
+
 
 export default configureStore;
--- myApp/src/store/reducers/places.js
+++ myApp/src/store/reducers/places.js
@@ -12,7 +12,7 @@ const reducer = (state = initialState, action) => {
             return {
                 ...state,
                 places: state.places.concat({
-                    key: Math.random(),
+                    key: Math.random().toString(36).substr(2, 9),
                     name: action.placeName,
                     image: {
                         uri: 'http://offroadbangladesh.com/wp-content/uploads/2015/02/Amiakum-waterfall-626x365.jpg'
