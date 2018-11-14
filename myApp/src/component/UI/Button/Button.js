cbeb5aae4269cd5a9eecc93449b799ced2391b0b
torikraju <torikraju@gamil.com>
2018-11-14 18:45:21 +0600

    101 Using the Platform API

--- myApp/src/component/UI/Button/Button.js
+++ myApp/src/component/UI/Button/Button.js
@@ -1,15 +1,28 @@
 import React from 'react';
-import {TouchableOpacity, Text, View} from 'react-native';
+import {TouchableOpacity, Text, View, TouchableNativeFeedback, Platform} from 'react-native';
 import styles from './Button.style';
 
 const Button = props => {
+
+    const content = (
+        <View style={[styles.button, {backgroundColor: props.color}]}>
+            <Text>
+                {props.children}
+            </Text>
+        </View>
+    );
+
+    if (Platform.OS === 'android') {
+        return (
+            <TouchableNativeFeedback onPress={props.onPress}>
+                {content}
+            </TouchableNativeFeedback>
+        );
+    }
+
     return (
         <TouchableOpacity onPress={props.onPress}>
-            <View style={[styles.button, {backgroundColor: props.color}]}>
-                <Text>
-                    {props.children}
-                </Text>
-            </View>
+            {content}
         </TouchableOpacity>
     );
 };
