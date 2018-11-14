5661d104c2179819fb2b9e00899a604efb7f9146
torikraju <torikraju@gamil.com>
2018-11-14 17:00:08 +0600

    093 Editing the Share Place Screen

--- myApp/src/screens/SharePlace/SharePlace.js
+++ myApp/src/screens/SharePlace/SharePlace.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, TextInput, Button, ScrollView, Image} from 'react-native';
 import {connect} from 'react-redux'
 
 import PlaceInput from '../../component/PlaceInput/PlaceInput';
@@ -7,6 +7,11 @@ import * as actions from '../../store/actions/index';
 import * as identifier from "../../Helper/identifires";
 import {iconsMap} from '../../Helper/iconHelper';
 import {NAVIGATION_IDENTIFIER} from '../../Helper/identifires';
+import {styles} from './SharePlace.style';
+import DefaultInput from '../../component/UI/DefaultInput/DefaultInput';
+import MainText from '../../component/UI/MainText/MainText';
+import HeadingText from '../../component/UI/HeadingText/HeadingText';
+import imagePlaceHolder from '../../assets/beautiful-place.jpg';
 
 
 class SharePlaceScreen extends Component {
@@ -32,9 +37,26 @@ class SharePlaceScreen extends Component {
 
     render() {
         return (
-            <View>
-                <PlaceInput onPlaceAdded={this.placeAddedHandler}/>
-            </View>
+            <ScrollView>
+                <View style={styles.container}>
+                    <MainText><HeadingText>Share a place with us!</HeadingText></MainText>
+                    <View style={styles.placeholder}>
+                        <Image source={imagePlaceHolder} style={styles.imagePreview}/>
+                    </View>
+                    <View style={styles.button}>
+                        <Button title='Pick Image'/>
+                    </View>
+                    <View style={styles.placeholder}><Text>Map</Text></View>
+                    <View style={styles.button}>
+                        <Button title='Locate Me'/>
+                    </View>
+                    <DefaultInput placeholder='Place Name'/>
+                    <View style={styles.button}>
+                        <Button title='Share the Place'/>
+                    </View>
+                    {/*<PlaceInput onPlaceAdded={this.placeAddedHandler}/>*/}
+                </View>
+            </ScrollView>
         );
     }
 }
---
+++ myApp/src/screens/SharePlace/SharePlace.style.js
@@ -0,0 +1,23 @@
+import {StyleSheet} from 'react-native';
+
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center'
+    },
+    placeholder: {
+        borderColor: 'black',
+        borderWidth: 1,
+        backgroundColor: '#eee',
+        width: '80%',
+        height: 150
+    },
+    button: {
+        margin: 8
+    },
+    imagePreview: {
+        width: '100%',
+        height: '100%'
+    }
+});
