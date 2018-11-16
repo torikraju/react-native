6428b12df6e78d2958a80e4be490a1a4f9c3dcfb
torikraju <torikraju@hotmail.com>
2018-11-16 15:30:51 +0600

    132 Picking a Location on the Map

--- myApp/src/component/PickLocation/PickLocation.js
+++ myApp/src/component/PickLocation/PickLocation.js
@@ -12,14 +12,31 @@ class PickLocation extends Component {
             latitudeDelta: 0.015,
             longitudeDelta: 0.0121
         }
-    }
+    };
+
+    pickLocationHanlder = event => {
+        const coordinate = event.nativeEvent.coordinate;
+
+        this.setState(prevState => {
+            return {
+                focusedLocation: {
+                    ...prevState.focusedLocation,
+                    latitude: coordinate.latitude,
+                    longitude: coordinate.longitude
+                }
+            };
+        });
+
+    };
 
     render() {
         return (
             <View style={styles.container}>
                 <MapView
                     initialRegion={this.state.focusedLocation}
+                    region={this.state.focusedLocation}
                     style={styles.map}
+                    onPress={this.pickLocationHanlder}
                 />
                 <View style={styles.button}>
                     <Button title='Locate Me' onPress={() => alert('located')}/>
